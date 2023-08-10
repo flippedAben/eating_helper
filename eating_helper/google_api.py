ad4b6a37d41f55707eca7c0bf1c997c8ecddc9bc
Ben Yang <ben.yang@utexas.edu>
2023-08-09 22:52:46 -0500

    Refresh token correctly

--- eating_helper/google_api.py
+++ eating_helper/google_api.py
@@ -1,11 +1,15 @@
+import logging
 import os.path
 
-from google.auth.transport.requests import Request
 from google.auth.exceptions import RefreshError
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 GOOGLE_API_CREDS = "google_api_credentials.json"
 
@@ -15,37 +19,35 @@ SCOPES = ["https://www.googleapis.com/auth/tasks"]
 
 def get_google_tasks_service():
     """
-    Return Google Tasks API usage object.
+    Authenticate, and return Google Tasks API usage object.
     """
     creds = None
-    # The file *_token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     token_json = "token.json"
     if os.path.exists(token_json):
         creds = Credentials.from_authorized_user_file(token_json, SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
+
+    if creds:
+        logger.info(
+            f"Got cached token. Status: {creds.expired=} {creds.refresh_token=}"
+        )
+        if creds.expired:
             try:
                 creds.refresh(Request())
+                logger.info(f"Refreshed token.")
             except RefreshError as e:
-                os.remove("token.json")
-                print("Removed token.json. Try again.")
+                logger.error("Failed to refresh token.")
                 raise e
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(GOOGLE_API_CREDS, SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(token_json, "w") as token:
-            token.write(creds.to_json())
+    else:
+        logger.info(f"Getting new token.")
+        flow = InstalledAppFlow.from_client_secrets_file(GOOGLE_API_CREDS, SCOPES)
+        creds = flow.run_local_server()
 
-    try:
-        service = build("tasks", "v1", credentials=creds)
-        return service
-    except HttpError as err:
-        print(err)
-        raise err
+    # Save the credentials for the next run
+    with open(token_json, "w") as token:
+        token.write(creds.to_json())
+
+    service = build("tasks", "v1", credentials=creds)
+    return service
 
 
 def clear_google_tasks(tasklist_id):
